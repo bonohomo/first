2a617dd84c505fce15bfc7e1004b3175f6fbe498
hedgehog <anishenko.fed@yandex.ru>
2026-09-06 16:12:59 +0300

    second

---
+++ src/Article.js
@@ -0,0 +1,11 @@
+import "./Article.css";
+function Article() { 
+    return (
+        <article>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptas repellat asperiores quaerat alias quia impedit tempore soluta, obcaecati maxime iste veritatis deserunt, recusandae, praesentium temporibus ipsam consequuntur vel id?</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates voluptas repellat asperiores quaerat alias quia impedit tempore soluta, obcaecati maxime iste veritatis deserunt, recusandae, praesentium temporibus ipsam consequuntur vel id?</p>
+      </article>
+    )
+}
+
+export default Article;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <p>Copyright</p>
+            </footer>
+        )
+    }
+
+}
+
+export default Footer;
